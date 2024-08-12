e0d6e67adc3a21b3623d9c2121142cdcb62ad2f4
SuhailAhamed29 <suhailahamedd27@gmail.com>
2024-08-12 15:40:39 +0530

    playlist5

---
+++ week1/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 14453
+let accountEmail = "khalid@google.com"
+var accountPassword = "12345"
+accountcity = "chennai"
+
+//accountId = 2
+
+console.log(accountId);
+
---
+++ week1/khalid.js
@@ -0,0 +1,7 @@
+let names = "khaid"
+let age = 21
+let add = "chennai"
+let place = true
+
+console.log(names,typeof age,add, typeof place);
+
